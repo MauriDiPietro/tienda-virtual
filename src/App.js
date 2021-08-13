5811f953cefed9f3cd5e1c0d86c684b53b4de1ff
MauriDiPietro <dipietro.jm@gmail.com>
2021-08-13 16:09:24 -0300

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
+import Header from './components/Header/Header'
+import Products from './components/Products/Products';
+import 'boxicons';
 
 function App() {
   return (
     <div className="App">
-     
+     <Header />
+     <Products />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import sportstore from '../../images/sportstore.png';
+
+function Header(){
+    return (
+        <header>
+            {/* <div className='menu'>
+                <box-icon name='menu'></box-icon>
+            </div> */}
+            <a href='#'>
+                <div className='logo'>
+                    <img src={sportstore} alt='' width='100'/>
+                </div>
+            </a>
+            <ul>
+                <li>
+                    <a href='#'>INICIO</a>
+                </li>
+                <li>
+                    <a href='#'>PRODUCTOS</a>
+                </li>
+            </ul>
+            <div className='cart'>
+                <box-icon name='cart'></box-icon>
+                <span className='item-total'>
+                    0
+                </span>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
